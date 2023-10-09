7eda8a5d8a58ac6df6b339a8c61566303037614a
Valeriia Baranivska <114081412+ValeriiaBaranivska@users.noreply.github.com>
2023-10-09 21:30:10 +0300

    Update Lab2_Baranivska.py

---
+++ Lab2/Lab2_Baranivska.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv('DS9.txt', sep=" ")
+
+x = data.iloc[:, 1:2]
+y = data.iloc[:, 0:1]
+
+plt.figure(figsize=(12, 6))  # 960x540 пікселів
+plt.scatter(x, y, s=10, c='purple', marker='')
+
+# Задаємо заголовок та підписи до осей
+plt.title('Графік точок')
+plt.xlabel('Вісь X')
+plt.ylabel('Вісь Y')
+plt.savefig('plot.png')# Зберігаємо результат у графічний формат
+plt.show()# Відображаємо графік на екрані
